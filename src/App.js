bbd14efe9c025d7e03877a349df156af83010e75
darshilkantaria11 <91153906+darshilkantaria11@users.noreply.github.com>
2022-04-15 22:42:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import Home from "./js/Home";
-import Second from "./js/Second";
-import Rotate from "./js/Rotate";
-import Liteabout from "./js/Liteabout";
-import Idea from "./js/Idea";
-import Offer from "./js/Offer";
-import News from "./js/News";
+import About from "./js/About";
+import Partner from "./js/Partner";
 import Event from "./js/Event";
+import Cources from "./js/Cources";
+
+import Navigation from "./js/Navigation";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
    <>
-       <Home/>
-       <Second/>
-       <Rotate/>
-       <Liteabout/>
-       <Offer/>
-       <Idea/>
-       <News/>
-       <Event/>
+       <Router>
+          
+             
+             <Routes>
+               <Route path="/" element={ <Home/>}/>
+               <Route path="/about" element={<About/>}/>
+               <Route path="/event" element={<Event/>}/>
+               <Route path="/partner" element={<Partner/>}/>
+               <Route path="/cources" element={<Cources/>}/>
+             </Routes>
+
+
+
+       </Router>
+
    </>
   );
 }
